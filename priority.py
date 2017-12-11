cfb995d1c54cfd37baa1503c2eafbaf812844626
Pandey <atharva.pandey@cgi.com>
2017-12-11 14:00:19 +0530

    done

--- priority.py
+++ priority.py
@@ -1,12 +1,18 @@
 import queue
 import time
+import threading
 
+def printing(idx):
+    print(idx)
+
+def thread(idx):
+    t=threading.Thread(target=printing,args=(idx, ))
+    t.start()
 q= queue.PriorityQueue()
-
-q.put((1,'priority 1'))
-q.put((3,'priority 3'))
-q.put((4,'priority 4'))
-q.put((2,'priority 2'))
+q.put((1,thread(1)))
+q.put((3,thread(3)))
+q.put((4,thread(4)))
+q.put((2,thread(2)))
 
 for i in range(q.qsize()):
-    print(q.get()[1])
+    q.get()[1]
